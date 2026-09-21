bd21e329f12ff1dda42ff0a1763d16b0b75d0ed0
LSbotbabe <all.star92506@gmail.com>
2026-09-21 15:49:10 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -75,13 +75,6 @@ client.on(Events.InteractionCreate, async (interaction) => {
     }
     return;
   }
-// Coastal Cutie sticker
-const stickerMessage = await interaction.guild.channels.cache
-  .get("1536069376485368018")
-  ?.messages.fetch("1551723534244515941")
-  .catch(() => null);
-
-const coastalCutieSticker = stickerMessage?.stickers.first();
 
   // Wave button
 if (
@@ -89,7 +82,12 @@ if (
   interaction.customId.startsWith("lit_wave_")
 ) {
   const newMemberId = interaction.customId.replace("lit_wave_", "");
+const stickerMessage = await interaction.guild.channels.cache
+  .get("1536069376485368018")
+  ?.messages.fetch("1551723534244515941")
+  .catch(() => null);
 
+const coastalCutieSticker = stickerMessage?.stickers.first();
   await interaction.reply({
   content: `👋 <@${interaction.user.id}> waved at <@${newMemberId}>!`,
   stickers: coastalCutieSticker ? [coastalCutieSticker.id] : []
